5abda7e80a8eec895017a0bda40a9d096b1b222f
MaLkoLm_X <jekyboyinhell@gmail.com>
2019-04-24 20:37:05 +0400

    Исправления

--- schema.sql
+++ schema.sql
@@ -6,14 +6,14 @@ USE yaticave;
 
 CREATE TABLE categories (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  name CHAR(30) UNIQUE,
+  name CHAR(60) UNIQUE,
   code CHAR(30) UNIQUE
 );
 
 CREATE TABLE lot (
   id INT AUTO_INCREMENT PRIMARY KEY,
   date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  name CHAR(30),
+  name CHAR(60),
   description TEXT,
   image CHAR(128),
   price INT(10),
@@ -36,7 +36,7 @@ CREATE TABLE user (
   id INT AUTO_INCREMENT PRIMARY KEY,
   date_reg TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   email CHAR(30) UNIQUE,
-  name CHAR(30) UNIQUE,
+  name CHAR(60) UNIQUE,
   password CHAR(30),
   avatar CHAR(128),
   contact CHAR(255)
